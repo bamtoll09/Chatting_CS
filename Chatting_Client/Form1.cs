a288b88fff35a99c8cfb1de5ceea80b9b133de26
bamtoll09 <youngwvwv@sju.ac.kr>
2021-03-21 13:30:47 +0900

    first commit

---
+++ Chatting_Client/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Chatting_Client
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientSocket = new TcpClient(); // 소켓
+        NetworkStream stream = default(NetworkStream);
+        string message = string.Empty;
+        static string nickname = "HE";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.Connect("localhost", 9999); // 접속 IP 및 포트
+                stream = clientSocket.GetStream();
+            }
+            catch (Exception e2)
+            {
+                MessageBox.Show("서버가 실행중이 아닙니다.", "연결 실패!");
+                Application.Exit();
+            }
+            message = "채팅 서버에 연결 되었습니다.";
+            DisplayText(message);
+            byte[] buffer = Encoding.Unicode.GetBytes(Form1.nickname + "$");
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+            Thread t_handler = new Thread(GetMessage);
+            t_handler.IsBackground = true;
+            t_handler.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+            byte[] buffer = Encoding.Unicode.GetBytes(textBox1.Text + "$");
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+            textBox1.Text = "";
+        }
+
+        private void GetMessage() // 메세지 받기
+        {
+            while (true)
+            {
+                stream = clientSocket.GetStream();
+                int BUFFERSIZE = clientSocket.ReceiveBufferSize;
+                byte[] buffer = new byte[BUFFERSIZE];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.Unicode.GetString(buffer, 0, bytes);
+                DisplayText(message);
+            }
+        }
+        private void DisplayText(string text) // Server에 메세지 출력
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new MethodInvoker(delegate
+                {
+                    richTextBox1.AppendText(text + Environment.NewLine);
+                }));
+            }
+            else
+                richTextBox1.AppendText(text + Environment.NewLine);
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // 엔터키 눌렀을 때
+                button1_Click(this, e);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            byte[] buffer = Encoding.Unicode.GetBytes("leaveChat" + "$");
+            stream.Write(buffer, 0, buffer.Length);
+            stream.Flush();
+            Application.ExitThread();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Chatting_Server/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Chatting_Server;
+
+namespace Chatting_Server
+{
+    public partial class Form1 : Form
+    {
+        private bool isConnected = false;
+        private bool isConnecting = false;
+
+        private TcpListener server = null;
+        private TcpClient clientSocket = null;
+
+        static int userCount = 0;
+        string date;
+
+        public Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ActiveControl = this.textBox1;
+
+            Thread thread = new Thread(InitSocket);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void InitSocket()
+        {
+            server = new TcpListener(IPAddress.Any, 9999);
+            clientSocket = default(TcpClient);
+            server.Start();
+            DisplayText(">> Server Started");
+
+
+            // Thread Loop
+            while (true)
+            {
+                try
+                {
+                    userCount++;
+                    clientSocket = server.AcceptTcpClient();
+                    DisplayText(">> Accept connection from client");
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string userName = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    userName = userName.Substring(0, userName.IndexOf("$"));
+
+                    clientList.Add(clientSocket, userName);
+
+                    SendMessageAll(userName + " 님이 입장하셨습니다.", "", false);
+
+                    handleClient hClient = new handleClient();
+                    hClient.OnReceived += new handleClient.MessageDisplayHandler(OnReceived);
+                    hClient.OnDisconnected += new handleClient.DisconnectedHandler(OnDisconnected);
+                    hClient.startClient(clientSocket, clientList);
+                }
+                catch (SocketException se) { break; }
+                catch (Exception ex) { break; }
+            }
+
+            clientSocket.Close();
+            server.Stop();
+        }
+
+        void OnDisconnected(TcpClient clientSocket)
+        {
+            if (clientList.ContainsKey(clientSocket))
+                clientList.Remove(clientSocket);
+        }
+
+        private void OnReceived(string message, string userName)
+        {
+            if (message.Equals("leaveChat"))
+            {
+                string displayMessage = "leave user: " + userName;
+                DisplayText(displayMessage);
+                SendMessageAll("leaveChat", userName, true);
+            } else {
+                string displayMessage = "From client: " + userName + " : " + message;
+                DisplayText(displayMessage);
+                SendMessageAll(message, userName, true);
+            }
+        }
+
+        public void SendMessageAll(string message, string userName, bool flag)
+        {
+            foreach (var pair in clientList)
+            {
+                date = DateTime.Now.ToString("MM.dd HH:mm:ss");
+
+                TcpClient client = pair.Key as TcpClient;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = null;
+
+                if (flag)
+                {
+                    if (message.Equals("leaveChat"))
+                        buffer = Encoding.Unicode.GetBytes(userName + " 님이 대화방을 나갔습니다.");
+                    else
+                        buffer = Encoding.Unicode.GetBytes("[" + date + "] " + userName + ": " + message);
+                }
+                else { buffer = Encoding.Unicode.GetBytes(message); }
+
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Flush();
+            }
+        }
+
+        private void DisplayText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                richTextBox1.BeginInvoke(new MethodInvoker(delegate
+                {
+                    richTextBox1.AppendText(text + Environment.NewLine);
+                }));
+            } else { richTextBox1.AppendText(text + Environment.NewLine); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+
+            textBox1.Text = String.Empty;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                MessageBox.Show(textBox1.Text);
+                textBox1.Text = String.Empty;
+            }
+        }
+    }
+}
